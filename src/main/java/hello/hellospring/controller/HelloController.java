bdf54fe6699826b4bc97a43ba8590d4932ff8a51
Yun-tellije <dbswjddms517@naver.com>
2024-01-25 18:56:19 +0900

    MVC - 컨트롤러 추가

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){ // 모델을 담으면 view에서 렌더링 할 때 사용
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
