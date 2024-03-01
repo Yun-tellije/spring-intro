421a27ccde70d5f7d97c63f8c971eba51dd4390f
Yun-tellije <dbswjddms517@naver.com>
2024-03-02 02:01:04 +0900

    comment: Spring Data JPA 설명

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -7,6 +7,10 @@ import java.util.Optional;
 
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
+    // JPQL select m from Member m where m.name = ?
+    // AND, OR 같은 거 사용 가능 ex) findByNameAndId(String name, Long id)
+    // -> 메서드 이름 만으로 조회하는 기능 제공
+    // 페이징 기능 제공
     @Override
     Optional<Member> findByName(String name);
 }
