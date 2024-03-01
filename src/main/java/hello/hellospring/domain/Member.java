170c1a107502eda20e1eb28c1f3168b26bf6f3fd
Yun-tellije <dbswjddms517@naver.com>
2024-03-01 22:54:02 +0900

    feat: Member - Entity 어노테이션 추가 및 id 설정

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,15 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // JPA가 관리하는 엔티티
 public class Member {
 
+    @Id // PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 시스템이 정해주는 아이디
     private String name;
 
